1d2e5b49758a4c568f0d34394c7ffc574b6e2fa5
renhongl <renhongl@ncs.com.sg>
2018-03-14 19:32:34 +0800

    update

--- the-best/src/component/footer/Footer.js
+++ the-best/src/component/footer/Footer.js
@@ -4,15 +4,34 @@ import React, { Component } from 'react';
 import { Icon } from 'antd';
 import './style.less';
 
+import { Setting } from '../setting';
+
 export default class Footer extends Component{
     constructor(props) {
         super(props);
+        this.toggleSetting = this.toggleSetting.bind(this);
+        this.state = {
+            showSetting: true
+        }
+    }
+
+    toggleSetting() {
+        debugger;
+        this.setState({
+            showSetting: !this.state.showSetting
+        });
     }
 
     render() {
         return (
             <footer className='footer'>
-                <Icon type="setting" />
+                <div className='left'>
+                    <Icon type="home" />
+                </div>
+                <div className='right'>
+                    <Setting className={this.state.showSetting ? 'show' : 'hide'}/>
+                    <Icon type="setting" onClick={this.toggleSetting}/>
+                </div>
             </footer>
         )
     }
